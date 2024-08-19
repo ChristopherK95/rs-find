a66b92b8022e9fcd36cd71743ed27b730152d856
ChristopherK95 <christopherkarlsson95@gmail.com>
2024-08-19 17:53:22 +0200

    Added case sensitivity as an option and a help command

--- src/main.rs
+++ src/main.rs
@@ -6,11 +6,50 @@ use std::{
     path::{Path, PathBuf},
 };
 
+struct Options {
+    case: bool,
+    help: bool,
+}
+
+impl Options {
+    fn new(args: &Vec<String>) -> Self {
+        let mut opt = Options::default();
+
+        for arg in args {
+            match arg.as_ref() {
+                "--case" => opt.case = true,
+                "--help" => opt.help = true,
+                _ => {}
+            }
+        }
+
+        return opt;
+    }
+}
+
+impl Default for Options {
+    fn default() -> Self {
+        Options {
+            case: false,
+            help: false,
+        }
+    }
+}
+
 fn main() -> io::Result<()> {
-    let args: Vec<String> = env::args().collect();
+    let mut args: Vec<String> = env::args().collect();
 
     let mut path_param: String = Default::default();
     let mut search: String = Default::default();
+    let options: Options = Options::new(&args);
+
+    if options.help {
+        println!("Help");
+
+        return Ok(());
+    }
+
+    clear_option_args(&mut args);
 
     validate_arguments(args, &mut path_param, &mut search);
 
@@ -20,12 +59,27 @@ fn main() -> io::Result<()> {
 
     let path = Path::new(&path_param);
 
-    iterate_dir(path, search.to_string())?;
+    let mut matches: Vec<String> = Vec::new();
+
+    iterate_dir(path, search.to_string(), &mut matches, options.case)?;
+
+    if matches.len() == 0 {
+        println!("No matches found");
+    } else {
+        for file_match in matches {
+            println!("{}", file_match);
+        }
+    }
 
     Ok(())
 }
 
-fn iterate_dir(path: &Path, search: String) -> io::Result<()> {
+fn iterate_dir(
+    path: &Path,
+    search: String,
+    matches: &mut Vec<String>,
+    case: bool,
+) -> io::Result<()> {
     for entry in fs::read_dir(path)? {
         match entry {
             Ok(entry) => {
@@ -42,9 +96,32 @@ fn iterate_dir(path: &Path, search: String) -> io::Result<()> {
                     None => continue,
                 }
                 if new_path.is_dir() && !is_read_only(&new_path) {
-                    iterate_dir(&new_path, search.to_string())?;
-                } else if entry.file_name() == OsString::from(&search) {
-                    println!("{:?}", new_path);
+                    iterate_dir(&new_path, search.to_string(), matches, case)?;
+                } else if entry.file_name() == OsString::from(&search) && case {
+                    match entry.file_name().into_string() {
+                        Ok(p) => matches.push(p),
+                        Err(_) => {}
+                    }
+                } else {
+                    let file_name = entry.file_name();
+                    let lower_case = entry.file_name().to_ascii_lowercase();
+                    match (
+                        case,
+                        file_name == OsString::from(&search),
+                        lower_case == OsString::from(&search).to_ascii_lowercase(),
+                    ) {
+                        (true, true, false) => match file_name.into_string() {
+                            Ok(s) => matches.push(s),
+                            Err(_) => {}
+                        },
+                        (false, true, true) | (false, false, true) => {
+                            match file_name.into_string() {
+                                Ok(s) => matches.push(s),
+                                Err(_) => {}
+                            }
+                        }
+                        _ => {}
+                    }
                 }
             }
             Err(err) => println!("BREEEH {}", err),
@@ -80,4 +157,8 @@ fn validate_arguments(args: Vec<String>, path_param: &mut String, search: &mut S
         }
         _ => println!("Too many arguments!"),
     }
+}
+
+fn clear_option_args(args: &mut Vec<String>) {
+    args.retain(|s| !s.starts_with("--"))
 }
